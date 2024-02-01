fed95e39010cac5726cfd40476e59637637a797d
Robert Santos <robertdahora@gmail.com>
2024-02-01 17:23:07 -0300

    adding viewsets

--- order/serializers/order_serializer.py
+++ order/serializers/order_serializer.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 
+from order.models import Order
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    product = ProductSerializer(required=True, many=True)
+    product = ProductSerializer(required=False, many=True)
+    products_id = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(), write_only=True, many=True
+    )
     total = serializers.SerializerMethodField()
 
     def get_total(self, instance):
@@ -14,4 +18,14 @@ class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ["product", "total"]
+        fields = ["product", "total", "products_id"]
+
+    def create(self, validated_data):
+        product_data = validated_data.pop("products_id")
+        user_data = validated_data.pop("user")
+
+        order = Order.objects.create(user_data=user_data)
+        for products in product_data:
+            order.product.add(products)
+
+        return order
