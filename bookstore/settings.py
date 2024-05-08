1264cac801ea5c267b76daa315225e5c4d53dcdc
Robert Santos <robertdahora@gmail.com>
2024-05-07 21:51:17 -0300

    fix: incorrect user database info

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
         "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("SQL_PASSWORD", "user"),
+        "USER": os.environ.get("SQL_USER", "user"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
